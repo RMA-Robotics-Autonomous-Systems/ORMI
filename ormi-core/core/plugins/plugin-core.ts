c64a76b31ec16b9732671dd982d1a08a72871cca
Florian Lebecque <Florianlebecque@hotmail.com>
2024-11-07 14:01:38 +0100

    The base plugin implement new basic functions for id

--- ormi-core/core/plugins/plugin-core.ts
+++ ormi-core/core/plugins/plugin-core.ts
@@ -3,25 +3,36 @@
 */
 
 
-import { PluginAction, PluginFilter, PluginData } from "@/core/plugins/plugins-types";
+import { PluginAction, PluginFilter, PluginData, PluginsHooks } from "@/core/plugins/plugins-types";
 
 abstract class PluginCore{
 
     protected name: string;
+    protected author: string;
+    protected email: string;
+    protected url: string;
+
     protected description: string;
     protected version: string;
 
+    protected dependencies: string[] = [];
 
 
     // actions
-    protected actions: Map<string, PluginAction> = new Map<string, PluginAction>();
-    protected filters: Map<string, PluginFilter> = new Map<string, PluginFilter>();
+    protected actions: Map<string | PluginsHooks , PluginAction> = new Map<string, PluginAction>();
+    protected filters: Map<string | PluginsHooks, PluginFilter> = new Map<string, PluginFilter>();
 
 
     constructor(){
         this.name = "core";
         this.description = "Core plugin";
         this.version = "1.0.0";
+        this.author = "";
+        this.email = ""
+        this.url = "";
+        
+
+
     }
 
     // abstract init(pl:PluginsLoader): void;
@@ -35,6 +46,22 @@ abstract class PluginCore{
 
     getVersion(): string{
         return this.version;
+    }
+
+    getAuthor(): string{
+        return this.author;
+    }
+
+    getEmail(): string{
+        return this.email;
+    }
+
+    getUrl(): string{
+        return this.url;
+    }
+
+    getDependencies(): string[]{
+        return this.dependencies;
     }
 
     toObject(): PluginData{
