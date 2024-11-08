bb251810ad8276dd48e7e10d5d01de38df2d4bab
Florian Lebecque <Florianlebecque@hotmail.com>
2024-11-08 11:17:03 +0100

    Implement JSON forms

--- ormi-core/core/widgets/widget-interface.ts
+++ ormi-core/core/widgets/widget-interface.ts
@@ -1,8 +1,18 @@
+import { JsonSchema, UISchemaElement } from "@jsonforms/core";
+
 interface WidgetDefinition {
     id: string;
     name: string;
     description: string;
     image: string;
+
+
+    schema: JsonSchema;
+    uischema: UISchemaElement;
+    data: object;
+
+
+    Component: (data:object) => JSX.Element;
 }
 
 
