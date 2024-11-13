7a2c9b9ce15ea3c3ba0e7985e2052d2793c2d447
FlorianLebecque <FlorianLebecque@hotmail.com>
2024-11-13 11:27:00 +0100

    Add ref to RRGL (prevent rerender and glitching when moving element)
    Add memo to the children; the content is not affected by the layout

--- ormi-core/core/dashboard/dashboard.tsx
+++ ormi-core/core/dashboard/dashboard.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { useState } from "react";
+import { useMemo, useRef } from "react";
 import { useDashboardManager } from "./dashboard-provider";
 import { Responsive, WidthProvider, Layout, Layouts } from "react-grid-layout";
-import { GearIcon, Cross1Icon } from "@radix-ui/react-icons"
+import { Cross1Icon } from "@radix-ui/react-icons"
 
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
@@ -14,49 +14,44 @@ import WidgetCard from "../widgets/components/widget-card/widget-card";
 
 const Dashboard = () => {
 
-    const { widgets, updateWidget, layouts, setLayouts, getComponents, getDefinition } = useDashboardManager();
+    const { widgets, updateWidget, removeWidget, layouts, setLayouts, getComponents, getDefinition } = useDashboardManager();
 
-    const ResponsiveGridLayout = WidthProvider(Responsive);
+    const ResponsiveGridLayout = useRef(WidthProvider(Responsive)).current;
 
-    const [previousLayout, setPreviousLayout] = useState<Layout[]>();
-
-    const getCurrentBreakpoint = (width: number) => {
-        if (width > 1200) {
-            return 'lg';
-        } else if (width > 996) {
-            return 'md';
-        } else if (width > 768) {
-            return 'sm';
-        } else if (width > 480) {
-            return 'xs';
-        } else {
-            return 'xxs';
+    const handleLayoutChange = (currentLayout: Layout[], allLayouts: Layouts) => {
+        if (JSON.stringify(layouts) !== JSON.stringify(allLayouts)) {
+            setLayouts({ ...allLayouts });
         }
     }
 
-    const handleLayoutsChange = (currentLayout: Layout[], allLayouts: Layouts) => {
+    const widgetsHTML = useMemo(() => {
 
-        // get the current breakpoint
-        const currentBreakpoint = getCurrentBreakpoint(window.innerWidth);
+        const handleRemoveBoxClick = (boxId: string, e: React.MouseEvent<HTMLButtonElement, HTMLElement>) => {
+            removeWidget(boxId);
+        }
 
-        // update the layout
-        // setLayouts({
-        //     ...layouts,
-        //     [currentBreakpoint]: currentLayout
-        // });
-    }
+        const handleSaveWidget = (box_id: string, widget: WidgetDefinition, settings: object) => {
+            updateWidget(box_id, settings);
+        }
 
-    const handleRemoveBoxClick = (boxId: string, e: React.MouseEvent<HTMLButtonElement, HTMLElement>) => {
-        console.log(`Remove Box ${boxId}`);
-    }
+        return Array.from(widgets.values()).map((widget: Widget) => {
+            return (
+                <div key={widget.box_id} className={style.widget + " shadow-md"}>
+                    <div className='flex flex-row content-between gap-1'>
+                        <div className={style.dragHandle}>{widget.title}</div>
+                        <WidgetCard data={widget.settings} definition={getDefinition(widget.widget_id)} displayGear={true} onValidate={(widget_def, settings) => { handleSaveWidget(widget.box_id, widget_def, settings) }} />
 
-    const handleSaveWidget = (box_id: string, widget: WidgetDefinition, settings: object) => {
-        console.log("box_id", box_id);
-        console.log("Widget selected", widget);
-        console.log("Widget settings", settings);
-
-        updateWidget(box_id, settings);
-    }
+                        <Button variant="destructive" onClick={(e) => handleRemoveBoxClick(widget.box_id, e)}>
+                            <Cross1Icon />
+                        </Button>
+                    </div>
+                    <div className={style.content}>
+                        {getComponents(widget.box_id)}
+                    </div>
+                </div>
+            );
+        });
+    }, [widgets, removeWidget, updateWidget, getDefinition, getComponents]);
 
     return (
         <ResponsiveGridLayout
@@ -67,28 +62,12 @@ const Dashboard = () => {
             breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
             cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
             draggableHandle={`.${style.dragHandle}`}
-            onLayoutChange={handleLayoutsChange}
+            onLayoutChange={handleLayoutChange}
             preventCollision={true}
             rowHeight={30}
             compactType={null}
         >
-            {Array.from(widgets.values()).map((widget: Widget) => {
-                return (
-                    <div key={widget.box_id} className={style.widget + " shadow-md"}>
-                        <div className='flex flex-row content-between gap-1'>
-                            <div className={style.dragHandle}>{widget.title}</div>
-                            <WidgetCard data={widget.settings} definition={getDefinition(widget.widget_id)} displayGear={true} onValidate={(widget_def, settings) => { handleSaveWidget(widget.box_id, widget_def, settings) }} />
-
-                            <Button variant="destructive" onClick={(e) => handleRemoveBoxClick(widget.box_id, e)}>
-                                <Cross1Icon />
-                            </Button>
-                        </div>
-                        <div className={style.content}>
-                            {getComponents(widget.box_id)}
-                        </div>
-                    </div>
-                );
-            })}
+            {widgetsHTML}
 
         </ResponsiveGridLayout>
     );
