e40c233b2d1a8b0a03c608cf6ca95f6ea43bf799
FlorianLebecque <FlorianLebecque@hotmail.com>
2024-11-18 15:10:54 +0100

    clean

--- ormi-core/components/advanced/navbar/navbar.tsx
+++ ormi-core/components/advanced/navbar/navbar.tsx
@@ -2,18 +2,11 @@
 import Link from "next/link";
 import { NavigationMenu, NavigationMenuList, NavigationMenuItem, NavigationMenuLink, navigationMenuTriggerStyle } from "../../ui/navigation-menu";
 
-import { WidgetsCombo } from "@/core/widgets/components/widget-combo/widget-combo";
-import { Button } from "../../ui/button";
-import { LockClosedIcon, LockOpen1Icon } from "@radix-ui/react-icons";
-import { useDashboardManager } from "@/core/dashboard/components/dashboard-provider";
 import { useNavbar } from "./navbar-provider";
-
 
 const NavBar = () => {
 
     const { left, center, right } = useNavbar();
-
-    console.log(left, center, right);
 
     return (
         <NavigationMenu className="shadow-md">
