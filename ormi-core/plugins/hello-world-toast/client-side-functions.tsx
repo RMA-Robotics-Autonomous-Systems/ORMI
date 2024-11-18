5eebe024b170a04a2c7d9cd9a16580f5ab067d6f
FlorianLebecque <FlorianLebecque@hotmail.com>
2024-11-18 14:50:34 +0100

    Remove hellow world toast but keep toaster

--- ormi-core/plugins/hello-world-toast/client-side-functions.tsx
+++ ormi-core/plugins/hello-world-toast/client-side-functions.tsx
@@ -1,20 +1,8 @@
 "use client";
 
 import { Toaster } from "@/components/ui/toaster"
-import { useToast } from "@/hooks/use-toast"
-import { useEffect } from "react";
-
 
 const HelloWorld = (elements: any) => {
-
-    const { toast } = useToast();
-
-    useEffect(() => {
-        toast({
-            title: "Hello World",
-            description: "This is a test",
-        });
-    }, []);
 
     return (
         <>
