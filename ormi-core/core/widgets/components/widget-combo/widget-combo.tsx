3016cf9118b8f622d8542fe0268adcb006ddb687
FlorianLebecque <FlorianLebecque@hotmail.com>
2024-11-18 14:59:02 +0100

    Clean

--- ormi-core/core/widgets/components/widget-combo/widget-combo.tsx
+++ ormi-core/core/widgets/components/widget-combo/widget-combo.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { ChevronsUpDown, Search } from "lucide-react"
+import { Search } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import {
