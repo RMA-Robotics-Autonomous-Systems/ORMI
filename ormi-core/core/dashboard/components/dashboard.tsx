b3f86435816bb9e335a95dc95237586557504024
FlorianLebecque <FlorianLebecque@hotmail.com>
2024-12-02 16:55:50 +0100

    Revert to memo

--- ormi-core/core/dashboard/components/dashboard.tsx
+++ ormi-core/core/dashboard/components/dashboard.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo } from "react";
 import { useDashboardManager } from '@/core/dashboard/components/dashboard-provider';
 import { Responsive, WidthProvider, Layout, Layouts } from "react-grid-layout";
 import { Cross1Icon, LockClosedIcon, LockOpen1Icon } from "@radix-ui/react-icons"
@@ -21,7 +21,7 @@ const Dashboard = () => {
 
     const { setNavbarItem, removeNavbarItem } = useNavbar();
 
-    const ResponsiveGridLayout = useRef(WidthProvider(Responsive)).current;
+    const ResponsiveGridLayout = useMemo(() => WidthProvider(Responsive), []);  // (improve performance from 'doc', also, juste make it works)
 
     const handleLayoutChange = (currentLayout: Layout[], allLayouts: Layouts) => {
         if (JSON.stringify(layouts) !== JSON.stringify(allLayouts)) {
