68bf2b8a945c25dc1a424fcb34228a85e8ef61b8
FlorianLebecque <FlorianLebecque@hotmail.com>
2024-12-04 14:58:29 +0100

    Move toaster inside the app itself

--- ormi-core/app/layout.tsx
+++ ormi-core/app/layout.tsx
@@ -5,6 +5,7 @@ import PluginsLoader from "@/core/plugins/plugins-loader";
 import { PluginsProvider } from "@/core/plugins/components/plugins-provider";
 import { NavbarProvider } from "@/components/advanced/navbar/navbar-provider";
 import NavBar from "@/components/advanced/navbar/navbar";
+import { Toaster } from "@/components/ui/toaster";
 
 // const geistSans = localFont({
 //     src: "./fonts/GeistVF.woff",
@@ -35,6 +36,7 @@ export default function RootLayout({
             <body>
                 <main>
                     <PluginsProvider pluginsLoader={pl.getClientSide()}>
+                        <Toaster />
                         <NavbarProvider>
                             <NavBar />
                             {children}
