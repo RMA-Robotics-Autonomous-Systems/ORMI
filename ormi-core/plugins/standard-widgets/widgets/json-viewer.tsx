5162e2dcafe2f915cdb7d645f109af165fde5ca9
Florian Lebecque <Florianlebecque@hotmail.com>
2024-12-05 13:57:22 +0100

    Make parent div a grid so the pre element take the full available height

--- ormi-core/plugins/standard-widgets/widgets/json-viewer.tsx
+++ ormi-core/plugins/standard-widgets/widgets/json-viewer.tsx
@@ -21,7 +21,7 @@ export function JsonViewer(props: any) {
     }, []);
 
     return (
-        <div style={{ height: "100%", overflow: "auto" }}>
+        <div style={{ height: "100%", overflow: "auto", display: "grid" }}>
             <pre className="shadow-inner-md rounded-md m-3 p-1" style={{ boxShadow: "5px 5px 16px 0px rgba(0,0,0,0.1) inset", backgroundColor: "darkslategrey", color: "white" }} >
                 {JSON.stringify(data, null, 2)}
             </pre>
