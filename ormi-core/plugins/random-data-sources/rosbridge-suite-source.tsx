2cffc02850415f04d1e3533c7c51d94a4c836a6d
FlorianLebecque <FlorianLebecque@hotmail.com>
2024-12-11 15:52:47 +0100

    WIP implementation of subscribe/unsubscribe to ROS2 bridge_suite

--- ormi-core/plugins/random-data-sources/rosbridge-suite-source.tsx
+++ ormi-core/plugins/random-data-sources/rosbridge-suite-source.tsx
@@ -73,9 +73,8 @@ async function GetTopicsList(ROS: ROSLIB.Ros): Promise<ROSTopic[]> {
 const RosBridgeSuiteSourceProvider: React.FC<{ children: ReactNode, props: RosBridgeSuiteDataSourceSettings }> = ({ children, props }) => {
     const pluginsManager = usePluginsManager() as PluginsManager;
 
-    // const ROS = useRef<ROSLIB.Ros>(new ROSLIB.Ros({
-    //     transportLibrary: 'websocket'
-    // })).current;
+    const subscribersRef = useRef(new Map<string, ROSLIB.Topic>());
+    const subscribersCountRef = useRef(new Map<string, number>());
 
     const datasource_id = props.id;
     const available_topics_handler = `${datasource_id}-available-topics`;
@@ -83,36 +82,118 @@ const RosBridgeSuiteSourceProvider: React.FC<{ children: ReactNode, props: RosBr
     const unsubscribe_hook = `${datasource_id}-unsubscribe`;
     const definition_hook = `${datasource_id}-definition`;
 
-    useEffect(() => {
 
-        const ROS = new ROSLIB.Ros({
-            transportLibrary: 'websocket'
+
+    useEffect(() => {
+        // const ROS = ROSRef.current;
+        const ROS = new ROSLIB.Ros(
+            {
+                transportLibrary: 'websocket',
+                url: props.url
+            }
+        );
+
+        pluginsManager.addFilter(PluginsHooks.AVAILABLE_TOPICS, {
+            id: available_topics_handler,
+            filter: async (topics: DatasourceTopic[]) => {
+
+                if (!ROS.isConnected) {
+                    return topics;
+                }
+
+                const rosTopics = await GetTopicsList(ROS);
+
+                for (const topic of rosTopics) {
+                    topics.push({
+                        topic: topic.topic,
+                        source: props,
+                        type: topic.type,
+                    });
+                }
+
+
+                return topics;
+            },
+            priority: 100
+        });
+
+        pluginsManager.addAction(subscribe_hook, {
+            id: subscribe_hook,
+            action: async (topic: DatasourceTopic) => {
+
+                if (!ROS.isConnected) {
+                    toast({
+                        title: "Error",
+                        description: "Could not connect to ROSBridge Suite",
+                        variant: "destructive"
+                    });
+                    return;
+                }
+
+                const topicType = await GetTopicType(ROS, topic.topic);
+
+                // check if the subscriber already exists, if yes increment the count
+                if (subscribersRef.current.has(topic.topic)) {
+                    subscribersCountRef.current.set(topic.topic, (subscribersCountRef.current.get(topic.topic) || 0) + 1);
+                    return;
+                }
+
+                const subscriber = new ROSLIB.Topic({
+                    ros: ROS,
+                    name: topic.topic,
+                    messageType: topicType
+                });
+
+                subscriber.subscribe((message: any) => {
+                    pluginsManager.doAction(`${datasource_id}-${topic.topic}-published`, message, Date.now());
+                });
+
+                subscribersRef.current.set(topic.topic, subscriber);
+                subscribersCountRef.current.set(topic.topic, 1);
+
+            },
+            priority: 100
+        });
+
+        pluginsManager.addAction(unsubscribe_hook, {
+            id: unsubscribe_hook,
+            action: async (topic: DatasourceTopic) => {
+
+                if (!ROS.isConnected) {
+                    return;
+                }
+
+                if (!subscribersRef.current.has(topic.topic)) {
+                    return;
+                }
+
+                const count = subscribersCountRef.current.get(topic.topic);
+                if (!count) {
+                    throw new Error(`Subscriber count not found for topic: ${topic.topic}`);
+                }
+
+                if (count === 1) {
+                    const subscriber = subscribersRef.current.get(topic.topic);
+                    if (!subscriber) {
+                        throw new Error(`Subscriber not found for topic: ${topic.topic}`);
+                    }
+                    subscriber.unsubscribe();
+                    subscribersRef.current.delete(topic.topic);
+                    subscribersCountRef.current.delete(topic.topic);
+                } else {
+                    subscribersCountRef.current.set(topic.topic, count - 1);
+                }
+            },
+            priority: 100
         });
 
         const connect = async () => {
 
             ROS.on("connection", () => {
-
-                pluginsManager.addFilter(PluginsHooks.AVAILABLE_TOPICS, {
-                    id: available_topics_handler,
-                    filter: async (topics: DatasourceTopic[]) => {
-
-                        const rosTopics = await GetTopicsList(ROS);
-
-                        for (const topic of rosTopics) {
-                            topics.push({
-                                topic: topic.topic,
-                                source: props,
-                                type: topic.type,
-                            });
-                        }
-
-
-                        return topics;
-                    },
-                    priority: 100
+                toast({
+                    title: "Connected to ROSBridge Suite",
+                    description: `Connected to ROSBridge Suite at ${props.url}`,
                 });
-
             });
 
             ROS.on("error", (error: any) => {
@@ -134,7 +215,7 @@ const RosBridgeSuiteSourceProvider: React.FC<{ children: ReactNode, props: RosBr
             });
 
             try {
-                await ROS.connect(props.url);
+                // await ROS.connect(props.url);
             } catch (error) {
                 console.error("Connection error:", error);
             }
@@ -143,9 +224,28 @@ const RosBridgeSuiteSourceProvider: React.FC<{ children: ReactNode, props: RosBr
         connect();
 
         return () => {
-            ROS.close();    // this function call the "close" event asynchronously
-
+            // the filter is only added when the connection is successful
             pluginsManager.removeFilter(available_topics_handler);
+            pluginsManager.removeFilter(definition_hook);
+
+            pluginsManager.removeAction(subscribe_hook);
+
+            // call the unsubscribe action for all the subscribers
+            subscribersRef.current.forEach((subscriber, topic) => {
+
+                pluginsManager.doAction(unsubscribe_hook, {
+                    topic: topic,
+                    source: props
+                });
+
+                subscriber.unsubscribe();
+
+            });
+
+            pluginsManager.removeAction(unsubscribe_hook);
+
+            ROS.close();
+
         };
     }, [props.url]); // Added props.url as dependency
 
