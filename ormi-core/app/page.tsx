35f169673a7da2e2333935f440e61872d9b822fe
FlorianLebecque <FlorianLebecque@hotmail.com>
2024-12-13 16:34:23 +0100

    set default dashboard to an empty dashboard

--- ormi-core/app/page.tsx
+++ ormi-core/app/page.tsx
@@ -8,60 +8,8 @@ import { Widget } from "@/core/widgets/widget-interface";
 
 import { Datasource } from "@/core/datasources/datasource-interface";
 import { GlobalDataSourcesProvider } from "@/core/datasources/components/global-datasource-provider";
-import { RandomDataSourceSettings } from "@/plugins/random-data-sources";
-import { RosBridgeSuiteDataSourceSettings } from "@/plugins/random-data-sources/rosbridge-suite-source";
 
 export default function Home() {
-
-    const datasources = new Map<string, Datasource>();
-    datasources.set('random-data-source', {
-        datasource_id: 'random-data-source',
-        title: 'Random Data Source',
-        settings: {
-            id: 'random-data-source',
-            title: "Random Data Source",
-            topics: [
-                {
-                    "topic": "/imu/vel/x",
-                    frequency: 16
-                },
-                {
-                    "topic": "/imu/vel/y",
-                    frequency: 32
-                },
-                {
-                    "topic": "/imu/vel/z",
-                    frequency: 64
-                }
-            ]
-        } as RandomDataSourceSettings
-    });
-
-    datasources.set('random-data-source-2', {
-        datasource_id: 'imu-data-source',
-        title: 'Random Data Source 2',
-        settings: {
-            id: 'random-data-source-2',
-            title: "Random Data Source 2",
-            topics: [
-                {
-                    "topic": "/camera/imu",
-                    frequency: 60
-                },
-            ]
-        } as RandomDataSourceSettings
-    });
-
-    datasources.set("local-rosbridge-suite", {
-        datasource_id: "rosbridge-suite-source",
-        title: "Local ROSBridge Suite",
-        settings: {
-            id: "local-rosbridge-suite",
-            title: "Local ROSBridge Suite",
-            url: "ws://localhost:9090",
-            reconnectTimeout: 5
-        } as RosBridgeSuiteDataSourceSettings
-    });
 
     const dashboardDefinition: DashboardInterface = {
         layouts: {
@@ -72,7 +20,7 @@ export default function Home() {
             xxs: []
         },
         widgets: new Map<string, Widget>(),
-        datasources: datasources,
+        datasources: new Map<string, Datasource>(),
         locked: false
     }
 
