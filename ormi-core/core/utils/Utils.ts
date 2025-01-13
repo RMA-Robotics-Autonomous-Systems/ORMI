2932f48bcb4837017a8c17873ef5fc92ee360250
Florian Lebecque <Florianlebecque@hotmail.com>
2025-01-13 17:28:57 +0100

    remove useless code

--- ormi-core/core/utils/Utils.ts
+++ ormi-core/core/utils/Utils.ts
@@ -1,15 +1,6 @@
-import { SelectedTopic } from "../jsonforms/topic-selector/topic-selector";
-
 export function generateUniqueID(): string {
     const timestamp = (new Date().getTime() / 1000 | 0).toString(16);
     const random = Math.random().toString(16).substring(2);
 
     return `${timestamp}-${random}`;
-}
-/*
-    parse : {"topic":"/cmd_vel","source":"rosbridge-suite-source","property":""}
-    to : SelectedTopic
-*/
-export function parseSelectedTopicJSON(topic: string): SelectedTopic {
-    return JSON.parse(topic) as SelectedTopic;
 }
