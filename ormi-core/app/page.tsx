dbd7b69d0806147da89e32ca5f2127795893b008
Florian Lebecque <Florianlebecque@hotmail.com>
2025-01-16 12:15:10 +0100

    comment WidgetAdder

--- ormi-core/app/page.tsx
+++ ormi-core/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
             <DashboardProvider dashboardDefinition={dashboardDefinition}>
                 <GlobalDataSourcesProvider>
                     <Dashboard />
-                    <WidgetsDialog />
+                    {/* <WidgetsDialog /> */}
                 </GlobalDataSourcesProvider>
             </DashboardProvider>
         </div>
