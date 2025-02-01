d01fe19e885b795f10be0a3e3c72867bf226217b
FlorianLebecque <FlorianLebecque@hotmail.com>
2025-02-01 04:17:51 +0100

    Implement speed indicator

--- ormi-core/plugins/flight-indicator/widget-export.tsx
+++ ormi-core/plugins/flight-indicator/widget-export.tsx
@@ -2,12 +2,14 @@
 
 import { WidgetDefinition } from '@/core/widgets/widget-interface';
 import { HeadingDefinition } from './indicators/heading/heading';
+import { AirspeedDefinition } from './indicators/airspeed/airspeed';
 
 
 
 const WidgetExport = (widgets: WidgetDefinition[]) => {
 
     widgets.push(HeadingDefinition());
+    widgets.push(AirspeedDefinition());
 
     return widgets;
 }
