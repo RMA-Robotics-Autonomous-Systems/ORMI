a9cc2c17068fbc4ee4326d51f80b3d0d77d259ec
FlorianLebecque <FlorianLebecque@hotmail.com>
2025-02-07 16:42:06 +0100

    Add documentation

--- ormi-core/core/widgets/widget-interface.ts
+++ ormi-core/core/widgets/widget-interface.ts
@@ -1,20 +1,21 @@
 import { JsonSchema, UISchemaElement } from "@jsonforms/core";
 
 interface WidgetDefinition {
-    id: string;
-    name: string;
-    description: string;
-    image?: string;
-
-
-    titleProp?: string;
-
-    schema: JsonSchema;
-    uischema: UISchemaElement;
-    data: any;
-
-    Component: (data:any) => JSX.Element;
-}
+    id: string;             // Id of the widgets, allow the dashboard to find which widgets is what component
+    name: string;           // name of the widget in the widget list
+    description: string;    // description of the widget
+    image?: string;         // Icon of the widget
+  
+    titleProp?: string; // A widget has multiple properties that are defined in the "schema" props, this allow the dashboard to find the property with the title
+  
+    //https://jsonforms.io/
+    schema: JsonSchema;         // a schema that describe the properties of the widget
+    uischema: UISchemaElement;  // describe how to display the properties in the settings section of the widgets
+    data: any;                  // default value of the widgets
+  
+    // component that will be put inside of the widget (the widget itself)
+    Component: (data: any) => JSX.Element;
+  }
 
 interface Widget{
     widget_id: string;  // point to the widget definition
