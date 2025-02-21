441d8e4e93e1e2a51f131882a7a38e22bc37ea1d
Florian Lebecque <Florianlebecque@hotmail.com>
2025-02-21 13:33:55 +0100

    Add boolean to status

--- ormi-core/plugins/standard-widgets/widgets/status/int-status-indicator.tsx
+++ ormi-core/plugins/standard-widgets/widgets/status/int-status-indicator.tsx
@@ -105,7 +105,7 @@ export function IntStatusIndicatorDefinition() {
                     scope: "#/properties/topic",
                     options: {
                         asyncFunction: async () => {
-                            return await pluginsManager.applyFilterAsync<DatasourceTopic[]>(PluginsHooks.AVAILABLE_TOPICS, [], new DatasourceTopicFilter({ type: /number/ }));
+                            return await pluginsManager.applyFilterAsync<DatasourceTopic[]>(PluginsHooks.AVAILABLE_TOPICS, [], new DatasourceTopicFilter({ type: /number|boolean/ }));
                         },
                         buffer: 1,
                     }
