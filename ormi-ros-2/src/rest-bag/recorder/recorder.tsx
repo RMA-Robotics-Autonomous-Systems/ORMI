b80c340539f425f2fa0db9ab964d3e516d43d46e
FlorianLebecque <FlorianLebecque@hotmail.com>
2025-03-24 15:30:17 +0100

    Clean

--- ormi-ros-2/src/rest-bag/recorder/recorder.tsx
+++ ormi-ros-2/src/rest-bag/recorder/recorder.tsx
@@ -1,5 +1,5 @@
 import { InfoIcon, SquareIcon } from "lucide-react";
-import { RecordingStatus, RecordingError } from "../recording-types";
+import { RecordingStatus } from "../recording-types";
 import { Badge, Button, Card, CardHeader, CardTitle, Table, TableBody, TableRow, TableCell, Dialog, DialogTrigger, DialogContent, DialogTitle, DialogDescription } from "ormi-core/components"
 import { RestBagClient } from "../rest-bag-client";
 import { useEffect, useState, useRef } from "react";
