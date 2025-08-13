8ca2e836c5d841b70f25caccef572af212c175b5
FlorianLebecque <FlorianLebecque@hotmail.com>
2025-08-13 14:43:11 +0200

    Increase capacity of topics filters

--- packages/ormi-core/src/datasources/datasource-interface.ts
+++ packages/ormi-core/src/datasources/datasource-interface.ts
@@ -42,7 +42,9 @@ interface SelectedTopic extends DatasourceTopic {
 interface DatasourceTopicFilterProps{
     name?: RegExp;      
     type?: RegExp;
+    rawType?: RegExp;
     source_id?: RegExp;
+    strict?: boolean; // if true, the filter returns true only if all properties match
 }
 
 // the filter works by using regexes on the name and type of the topic
@@ -50,23 +52,39 @@ class DatasourceTopicFilter {
     name?: RegExp;      
     type?: RegExp;
     source_id?: RegExp;
+    rawType?: RegExp;
+    strict?: boolean;
 
     constructor(props: DatasourceTopicFilterProps) {
         this.name = props.name;
         this.type = props.type;
         this.source_id = props.source_id
+        this.rawType = props.rawType;
+        this.strict = props.strict || false; // if true, the filter returns true only if
     }
 
     filter(topic: DatasourceTopic) : boolean {
+
+
+        const matches = [];
+
         if(this.name && !this.name.test(topic.topic)){
-            return false;
+            matches.push(false);
         }
 
         if(this.type && !this.type.test(topic.type)){
-            return false;
+            matches.push(false);
         }
 
         if(this.source_id && !this.source_id.test(topic.datasource_id)){
+            matches.push(false);
+        }
+
+        if(this.rawType && !this.rawType.test(topic.rawType)){
+            matches.push(false);
+        }
+
+        if(this.strict && matches.length > 0){
             return false;
         }
 
