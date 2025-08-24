e47289a071783bb868edcf3b19b341a2b8b64176
FlorianLebecque <FlorianLebecque@hotmail.com>
2025-08-24 13:53:42 +0200

    Remove the pluginManager from the dep array, this prevent unwanted rerender

--- packages/ormi-core/src/datasources/components/publisher-datasource-provider.tsx
+++ packages/ormi-core/src/datasources/components/publisher-datasource-provider.tsx
@@ -218,7 +218,7 @@ const PublisherDataSourcesProvider = (props: PublisherDataSourcesProviderProps) 
         };
 
         // Rerun effect if SelectedTopics change
-    }, [SelectedTopics, pluginsManager]);
+    }, [SelectedTopics]);
 
 
     return (
