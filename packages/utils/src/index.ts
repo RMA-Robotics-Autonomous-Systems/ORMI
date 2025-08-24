a60427d53156c20c9116f21b11038f05c7c4dded
FlorianLebecque <FlorianLebecque@hotmail.com>
2025-08-24 13:19:29 +0200

    Add a websocket provider for future use

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -2,5 +2,6 @@ export * from './utils';
 
 export * from './cookies/cookies-provider';
 export * from './local-storage/local-storage-provider'
+export * from './websocket/websocket-provider';
 
 export * from './colors';
